42db0da318462f12e18b4d6c5b26dbaedb8776f4
bsivanov <bsivanov@gmail.com>
2013-11-20 01:24:27 +0200

    Added initial source code.

---
+++ ClearTrackedChanges/ClearTrackedChanges/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace BorislavIvanov.ClearTrackedChanges
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidClearTrackedChanges =        0x100;
+
+
+    };
+}
